99afef52abb4a5d611db9955f108219c9db56fd0
Yulun Tian <yulun@mit.edu>
2020-09-17 14:49:33 -0400

    rename ROS nodes

--- src/DistributedLoopClosureNode.cpp
+++ src/DistributedLoopClosureNode.cpp
@@ -6,12 +6,10 @@
 
 #include <ros/ros.h>
 #include <kimera_distributed/DistributedLoopClosure.h>
-#include <kimera_distributed/KimeraDistributed.h>
-
 using namespace kimera_distributed;
 
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "kimera_distributed_node");
+  ros::init(argc, argv, "kimera_distributed_loop_closure_node");
   ros::NodeHandle nh;
 
   DistributedLoopClosure dlc(nh);
